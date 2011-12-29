35a7061a12c2b142ab9be4b6813a018269155120
Sergio Cerrutti <info@sergiocerrutti.com>
2011-12-29 11:13:25 +0100

    Removing useless <div> wrapping content inside drupal forms. Added code comments to template.php.

--- template.php
+++ template.php
@@ -1,6 +1,19 @@
 <?php
 
-function nombreDelTheme_preprocess_node(&$vars) {
-	// We create a new theme suggestion for node teasers
+/**
+ * Implementation of node preprocess
+ */
+function yourtheme_preprocess_node(&$vars) {
+	// Creating new theme suggestion for node teasers
 	$vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__' . $vars['view_mode'];
+}
+
+/**
+ * Implementation of theme('form')
+ */
+function yourtheme_form($variables) {
+  $element = $variables['element'];
+  
+  // Removing anonymous and useless <div> wrapping inside forms
+  return '<form' . drupal_attributes($element['#attributes']) . '>' . $element['#children'] . '</form>';
 }
