2b52e184a70e737ce5165d0da9d5523f1360beac
Juan Pastor <juantoleillo@gmail.com>
2012-05-24 13:39:41 +0200

    Allow contextual links for administrative users.

--- templates/core/block/block.tpl.php
+++ templates/core/block/block.tpl.php
@@ -1,3 +1,16 @@
-<div id="<?php print $block_html_id; ?>" class="block"<?php print $attributes; ?>>
-  <?php print $content ?>
-</div>
+<?php if(user_access('administer blocks')): ?>
+  <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+      <?php print render($title_prefix); ?>
+      <?php if ($block->subject): ?>
+        <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+      <?php endif;?>
+      <?php print render($title_suffix); ?>
+<?php endif; ?>
+
+    <div id="<?php print $block_html_id; ?>" class="block"<?php print $attributes; ?>>
+      <?php print $content ?>
+    </div>
+
+<?php if(user_access('administer blocks')): ?>
+  </div>
+<?php endif; ?>
